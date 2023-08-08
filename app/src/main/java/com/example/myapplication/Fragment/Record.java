cab9bf3430ac3085a17569decaad23f39a31a86e
yitin0826 <yitin0826@gmail.com>
2023-08-08 10:37:35 +0800

    Category變動

--- app/src/main/java/com/example/myapplication/Fragment/Record.java
+++ app/src/main/java/com/example/myapplication/Fragment/Record.java
@@ -36,11 +36,7 @@ import java.util.Date;
 
 public class Record extends Fragment implements RecordAdapter.OnItemListener, MariaDBCallback {
 
-<<<<<<< HEAD
-    //    private RecyclerView recycler_category, recycler_detail;
-=======
 //    private RecyclerView recycler_category, recycler_detail;
->>>>>>> ed76c3f (新增Category頁面)
 //    private RecyclerView.Adapter adapter_category, adapter_detail;
     private EditText edit_month;
     private RadioButton radiobtn_o2n, radiobtn_n2o;
@@ -109,21 +105,13 @@ public class Record extends Fragment implements RecordAdapter.OnItemListener, Ma
 
     }
 
-<<<<<<< HEAD
-    public void createMonthDialog(final EditText edt) {
-=======
     /** 選擇月份 **/
     public void createMonthDialog(final EditText edt){
->>>>>>> ed76c3f (新增Category頁面)
         MonthPickDialog dialog = new MonthPickDialog(getActivity());
         edt.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-<<<<<<< HEAD
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-=======
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
->>>>>>> ed76c3f (新增Category頁面)
                     dialog.showDialog();
                     return true;
                 }
@@ -139,19 +127,6 @@ public class Record extends Fragment implements RecordAdapter.OnItemListener, Ma
         };
     }
 
-<<<<<<< HEAD
-    private void RecyclerViewRecord() {
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),
-                LinearLayoutManager.VERTICAL, false);
-        recycler_record.setLayoutManager(linearLayoutManager);
-
-        ArrayList<DataRecord> recordList = new ArrayList<>();
-        recordList.add(new DataRecord("2023-07-19", "17:00"));
-        recordList.add(new DataRecord("2023-07-19", "17:05"));
-        recordList.add(new DataRecord("2023-07-19", "17:10"));
-        recordList.add(new DataRecord("2023-07-19", "17:15"));
-        recordList.add(new DataRecord("2023-07-19", "17:20"));
-=======
     /** 紀錄顯示 **/
     private void RecyclerViewRecord(){
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
@@ -163,7 +138,6 @@ public class Record extends Fragment implements RecordAdapter.OnItemListener, Ma
         recordList.add(new DataRecord("2023-07-19","17:10"));
         recordList.add(new DataRecord("2023-07-19","17:15"));
         recordList.add(new DataRecord("2023-07-19","17:20"));
->>>>>>> ed76c3f (新增Category頁面)
 
         adapter_record = new RecordAdapter(recordList, this);
         recycler_record.setAdapter(adapter_record);
@@ -295,17 +269,6 @@ public class Record extends Fragment implements RecordAdapter.OnItemListener, Ma
 
 }
 
-<<<<<<< HEAD
-class MonthPickDialog {
-    private Activity activity;
-    OnDialogRespond onDialogRespond;
-
-    public MonthPickDialog(Activity activity) {
-        this.activity = activity;
-    }
-
-    public void showDialog() {
-=======
 class MonthPickDialog{
     private Activity activity;
     OnDialogRespond onDialogRespond;
@@ -315,7 +278,6 @@ class MonthPickDialog{
     }
 
     public void showDialog(){
->>>>>>> ed76c3f (新增Category頁面)
         Dialog monthDialog = new Dialog(this.activity, R.style.MonthDialog);
         View contentView = LayoutInflater.from(this.activity).inflate(R.layout.dialog_month, null);
         monthDialog.setContentView(contentView);
@@ -326,13 +288,8 @@ class MonthPickDialog{
         monthDialog.getWindow().setWindowAnimations(R.style.dialogWindowAnim);
         monthDialog.show();
 
-<<<<<<< HEAD
-        NumberPicker np_year, np_month;
-        Button btn_monthCancel, btn_monthDone;
-=======
         NumberPicker np_year,np_month;
         Button btn_monthCancel,btn_monthDone;
->>>>>>> ed76c3f (新增Category頁面)
         np_year = contentView.findViewById(R.id.np_year);
         np_month = contentView.findViewById(R.id.np_month);
         btn_monthCancel = contentView.findViewById(R.id.btn_monthCancel);
@@ -341,13 +298,10 @@ class MonthPickDialog{
         Date date = new Date();
         int year = calendar.get(Calendar.YEAR);
 
-<<<<<<< HEAD
         np_year.setMaxValue(year + 20);
         np_year.setMinValue(year - 20);
-=======
         np_year.setMaxValue(year+20);
         np_year.setMinValue(year-20);
->>>>>>> ed76c3f (新增Category頁面)
         np_year.setValue(Integer.parseInt(new SimpleDateFormat("yyyy").format(date)));
         np_month.setMaxValue(12);
         np_month.setMinValue(1);
@@ -362,22 +316,14 @@ class MonthPickDialog{
         btn_monthDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-<<<<<<< HEAD
-                String s = String.format("%04d-%02d", np_year.getValue(), np_month.getValue());
-=======
                 String s = String.format("%04d-%02d",np_year.getValue(),np_month.getValue());
->>>>>>> ed76c3f (新增Category頁面)
                 onDialogRespond.onRespond(s);
                 monthDialog.dismiss();
             }
         });
     }
 
-<<<<<<< HEAD
-    interface OnDialogRespond {
-=======
     interface OnDialogRespond{
->>>>>>> ed76c3f (新增Category頁面)
         void onRespond(String selected);
     }
 }
