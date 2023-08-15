96898c9d2aff974faa6f2483e4f0906247628116
yitin0826 <yitin0826@gmail.com>
2023-08-15 10:31:21 +0800

    record畫面更改

--- app/src/main/java/com/example/myapplication/Data/DataRecord.java
+++ app/src/main/java/com/example/myapplication/Data/DataRecord.java
@@ -3,14 +3,17 @@ package com.example.myapplication.Data;
 import androidx.lifecycle.ViewModel;
 
 public class DataRecord extends ViewModel {
-    private String date,time,preloadData;
+    private String date,time,heartbeat,index,brain,preloadData;
 
     public DataRecord() {
     }
 
-    public DataRecord(String date,String time){
+    public DataRecord(String date,String time,String heartbeat,String index,String brain){
         this.date = date;
         this.time = time;
+        this.heartbeat = heartbeat;
+        this.index = index;
+        this.brain = brain;
     }
     public void setData(String newData) {
         preloadData = newData;
